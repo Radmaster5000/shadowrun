bfa9f085e935e6d587162279d2f9f3d12f97f62b
Radmaster5000 <phil.radford5000@gmail.com>
2021-06-05 23:10:42 +0100

    added two more skeleton functions. Updated README

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # shadowrun
+
+Trying to make a text based Shadowrun game. Premise is that you're a decker and you're doing runs. Action mechanics are based on the Shadowrun RPG, or they will be once they're up and running.
+
+**************************
+
+Stages of game so far:
+
+* sneak inside
+* initial log in
+* searching
+* task
+* logout
+* escape
+
+****************
+
+Functions defined so far:
+
+intro()
+rules()
+sneak_inside()
+initial_log_in()
--- shadowrun.py
+++ shadowrun.py
@@ -9,23 +9,23 @@ def intro(key):
 
 		print("""
 
-		#### #  # #### ###  #### #     # #### #  # #   #
-		#    #  # #  # #  # #  # #     # #  # #  # ##  #
-		#### #### #### #  # #  # #  #  # #### #  # # # # 
-		   # #  # #  # #  # #  # # # # # # #  #  # #  ##
-		#### #  # #  # ###  #### ### ### #  # #### #   #
+	#### #  # #### ###  #### #     # #### #  # #   #
+	#    #  # #  # #  # #  # #     # #  # #  # ##  #
+	#### #### #### #  # #  # #  #  # #### #  # # # # 
+	   # #  # #  # #  # #  # # # # # # #  #  # #  ##
+	#### #  # #  # ###  #### ### ### #  # #### #   #
 
-		         		Welcome to Shadowrun!
-		 Take the role of a Decker and choose your run!
+	     		Welcome to Shadowrun!
+	Take the role of a Decker and choose your run!
 
-		************************************************
+	************************************************
 
-						SELECT AN OPTION:
-						1 - Start game
-						2 - How to play
-						3 - Quit
+			SELECT AN OPTION:
+			1 - Start game
+			2 - How to play
+			3 - Quit
 
-		************************************************
+	************************************************
 		   """)
 
 		try:
@@ -64,7 +64,65 @@ def rules():
 		time.sleep(1)
 		rules()
 
+def sneak_inside():
+	print("""
+	*****************************************
+	             GET INSIDE
+	*****************************************
+	
+	*** FLAVOUR TEXT ABOUT SNEAKING IN ***
+
+		SELECT AN OPTION
+		1 - Sneak past guard
+		2 - Take down guard
+
+	""")
+
+	choice = 0
+
+	while (choice != 1 or choice != 2):
+		try:
+			choice = int(input('>>> '))
+			if (choice == 1 or choice == 2):
+				return choice
+			else:
+				print('ooops! Try again!')
+		except ValueError:
+			print('Type a number, stupid!')
+
+def initial_log_in():
+	print("""
+	*****************************************
+	             COMPROMISE SYSTEM
+	*****************************************
+	
+	*** FLAVOUR TEXT ABOUT LOGGING IN ***
+
+		SELECT AN OPTION
+		1 - Brute force password
+		2 - Use professional crypto program
+		3 - Use custom program""")
+	if (choice == 2):
+		print("		4 - Use guard's details (HIDDEN)")
+
+	hack1 = 0
+
+	while (hack1 != 1 or hack1 != 2):
+		try:
+			hack1 = int(input('>>> '))
+			if (hack1 == 1 or hack1 == 2 or hack1 == 3 or hack1 == 4):
+				return hack1
+			else:
+				print('ooops! Try again!')
+		except ValueError:
+			print('Type a number, stupid!')
+
+
 
 option = intro(0)
+choice = sneak_inside()
+log_in = initial_log_in()
 
-print(option)
+print("Intro option = " + str(option))
+print("Sneak Inside option = " + str(choice))
+print("Initial log in option = " + str(log_in))
